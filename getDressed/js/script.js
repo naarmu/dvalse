0a2d1bebdf69824fc9c080813e01881dff126dcc
taehotedgim <taehotedgim@gmail.com>
2021-10-09 00:17:31 +0700

    Update script.js

--- getDressed/js/script.js
+++ getDressed/js/script.js
@@ -3,21 +3,21 @@ var $elsArr = [].slice.call(document.querySelectorAll('.el'));
 var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
 
 setTimeout(function() {
-  $cont.classList.remove('s--inactive');
+  $cont.classList.remove('inactive');
 }, 200);
 
 $elsArr.forEach(function($el) {
   $el.addEventListener('click', function() {
-    if (this.classList.contains('s--active')) return;
-    $cont.classList.add('s--el-active');
-    this.classList.add('s--active');
+    if (this.classList.contains('active')) return;
+    $cont.classList.add('el-active');
+    this.classList.add('active');
   });
 });
 
 $closeBtnsArr.forEach(function($btn) {
   $btn.addEventListener('click', function(e) {
     e.stopPropagation();
-    $cont.classList.remove('s--el-active');
-    document.querySelector('.el.s--active').classList.remove('s--active');
+    $cont.classList.remove('el-active');
+    document.querySelector('.el.active').classList.remove('active');
   });
 });
